f0ac8fb3e8b6eb70941cdd9dd9def8f86e1eb1ea
MohamedMorsou <usersimo1997@gmail.com>
2024-05-22 23:13:53 +0100

    add feature sectoin and our great doctors

---
+++ frontend/src/components/Doctors/DoctorList.jsx
@@ -0,0 +1,21 @@
+import DoctorCard from './DoctorCard'
+// import useFecthData from '../../hooks/useFecthData'
+// import Loading from '../Loader/Loading'
+// import Error from '../Error/Error'
+// import { BASE_URL } from '../../config'
+import {doctors} from './../../assets/data/doctors';
+
+const DoctorList = () => {
+  return (
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px] ">
+        {doctors.map(doctor => (
+            <DoctorCard key={doctor.id} doctor={doctor} />
+        ))}
+
+      </div>
+    </>
+  )
+}
+
+export default DoctorList
